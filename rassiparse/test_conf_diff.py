df34832a0a5e351f2f385844d38204649ee7462d
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2017-03-30 12:21:13 +0200

    added second test

---
+++ rassiparse/test_conf_diff.py
@@ -0,0 +1,13 @@
+from rassiparse.rassiparse import conf_diff
+
+def test_conf_diff():
+    c1 = "22222 00000"
+    c2 = "2222u d0000"
+    assert(set(conf_diff(c1, c2)) == set([(4, 5)]))
+
+    c2 = "22220 du000"
+    assert(set(conf_diff(c1, c2)) == set([(4, 5), (4, 6)]))
+
+    # Ambiguous transitions
+    c2 = "22200 dudu0"
+    assert(set(conf_diff(c1, c2)) == set([]))
